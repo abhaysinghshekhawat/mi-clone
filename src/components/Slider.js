51245604b8610e30c742d7e977c7b65081194612
abhaysinghshekhawat <abhaysinghjprk6@gmail.com>
2022-02-06 18:38:44 +0530

    updated some styles

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Carousel from 'react-bootstrap/Carousel'
+import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
 
 const Slider = ({start}) => {
   return (
-    <Carousel fade>
+    <Carousel fade id="carousel">
      
          {
              start.map((item,idx)=>{
--- src/components/StarProducts.js
+++ src/components/StarProducts.js
@@ -9,15 +9,15 @@ const StarProducts = ({starProduct}) => {
 
            <div className="sp-inner-1">
             <div  >
-                <a href={starProduct[1].url}><img src={starProduct[1].image} alt="" /></a>
+                <a href={starProduct[1].url}><img  className='up' src={starProduct[1].image} alt="" /></a>
             </div>
 
             <div  >
-                <a href={starProduct[2].url}><img src={starProduct[2].image} alt="" /></a>
+                <a href={starProduct[2].url}><img  className='up' src={starProduct[2].image} alt="" /></a>
             </div>
 
             <div  >
-                <a href={starProduct[3].url}><img src={starProduct[3].image} alt="" /></a>
+                <a href={starProduct[3].url}><img className='lower' src={starProduct[3].image} alt="" /></a>
             </div>
           
            </div>
